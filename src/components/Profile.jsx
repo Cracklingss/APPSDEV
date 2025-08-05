ff5a9747d7a806b51874894446493be9fa00407c
Cracklingss <catubayfrancis7@gmail.com>
2025-08-05 12:49:39 +0800

    practice

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,27 +5,12 @@ import Button from "./Button"
 
 
 function Profile() {
-    const profile = {
-        profpic : "my-profile.jpg",
-        name : "Jessie Francis Catubay",
-        aboutMe : "Hi my name is Jessie Francis P. Catubay a BSIT Student at Cordova Public College, I am 19 years old and I live in Day-as Cordova Cebu. A little bit about myself here is I love playing online games such as Mobile Legends, Call of Duty, and Chess. And of course I love school too, and hoping to be a backend developer in the future.",
-        job : "Software Developer",
-        socialLink : "https://www.facebook.com/itsmecracklings"
-    };
-
     return (
         <div className="profile-container">
-            <img src={profile.profpic} alt="ProfilePic" width="400" height="400"/>
-            <h3>{profile.name}</h3>
-            <p>{profile.job}</p>
-            <div>
-                <h1>About Me</h1>
-                <p>{profile.aboutMe}</p>
-            </div>
+            <ProfilePicture />
+            <AboutMe />
             <Skills />
-            <a href={profile.socialLink}>
-                <button>Social</button>
-            </a>
+            <Button />
         </div>
     )
 }
